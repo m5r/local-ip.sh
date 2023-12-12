5b6c21ef6d33f44da8c7ba00c2712d845c6641d4
m5r <mokht@rmi.al>
2023-12-12 23:33:14 +0100

    add proper content type to force the browser to download the files instead of rendering their content

--- http/server.go
+++ http/server.go
@@ -1,15 +1,19 @@
 package http
 
 import (
+	"log"
 	"net/http"
 )
 
 func ServeCertificate() {
 	http.HandleFunc("/server.key", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/octet-stream")
 		http.ServeFile(w, r, "/certs/server.key")
 	})
 	http.HandleFunc("/server.pem", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/x-x509-ca-cert")
 		http.ServeFile(w, r, "/certs/server.pem")
 	})
+	log.Printf("Serving cert files on :9229\n")
 	http.ListenAndServe(":9229", nil)
 }
